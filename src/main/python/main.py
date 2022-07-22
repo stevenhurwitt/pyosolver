cdf9dc9330d50afc864d799d234de848159aca6a
steven hurwitt <stevenhurwitt@gmail.com>
2022-07-22 13:12:35 -0500

    update main.py

---
+++ src/main/python/main.py
@@ -0,0 +1,17 @@
+import pprint
+import json
+import time
+import sys
+import os
+
+base = os.getcwd()
+pp = pprint.PrettyPrinter(indent = 1)
+print("imported modules.")
+
+def main():
+    print("main")
+    pass
+
+if __name__ == "__main__":
+    main()
+    
