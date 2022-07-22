57b65a8779f7475bcfcf8548e82a0e971194837e
steven hurwitt <stevenhurwitt@gmail.com>
2022-07-22 16:22:35 -0500

    main function scala

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -10,9 +10,10 @@ object main {
         val AWS_ACCESS_KEY_ID = ""
         val AWS_SECRET_ACCESS_KEY = ""
         println("subreddit: " + subreddit)
-        return(subreddit)
+        // return(subreddit)
     }
 
-    val sureddit = main()
-    println("subreddit: " + subreddit.toString)
+    main()
+    // val sureddit = main()
+    // println("subreddit: " + subreddit.toString)
 }
