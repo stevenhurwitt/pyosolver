c4b601c07ffc709a9837b7e80ab9ccaf64292897
steven hurwitt <stevenhurwitt@gmail.com>
2022-07-22 15:47:44 -0500

    main scala

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,15 @@
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql._
 
-def main():
+def main(): Unit = {
     println("main")
 
-main()
+    val subreddit = "technology"
+    val AWS_ACCESS_KEY_ID = ""
+    val AWS_SECRET_ACCESS_KEY = ""
+    println("subreddit: " + subreddit)
+    return(subreddit)
+}
+
+// val sureddit = main()
